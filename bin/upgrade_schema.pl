e63df90cb1910901e6cabb6fb504e45a03eb3cb6
Jose Luis Martinez <jlmartinez@capside.com>
2016-05-12 09:17:21 +0000

    Update upgrade_schema to new standards

--- bin/upgrade_schema.pl
+++ bin/upgrade_schema.pl
@@ -1,23 +1,45 @@
 #!/usr/bin/env perl
-# ABSTRACT: Given an environment with a ENZiME DB and its related schema classes,
-# this script upgrades the schema of the DB to match the version of DBIx schema classes
-# with DBIx::Class::DeploymentHandler upgrade scripts
+
 use strict;
 use warnings;
 use feature qw/say/;
 
 use DBIx::Class::DeploymentHandler;
-use FindBin;
-use lib "$FindBin::Bin/../../lib", "$FindBin::Bin/../../../lib";
-use ENZiMEDB;
 
+package UpgradeDBFlow::Args {
+  use Moose;
+  with 'MooseX::Getopt';
+  use Module::Runtime qw/require_module/;
 
-my $schema = ENZiMEDB->connect("DBI:mysql:mysql_read_default_file=$ENV{HOME}/.dbadmin.cnf;mysql_read_default_group=enzime", undef, undef);
+  has schema => (is => 'ro', isa => 'Str', required => 1, documentation => 'The name of the schema class to load. Must be a DBIx::Class');
+
+  has _schema => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+      my $self = shift;
+      my $schema_name = $self->schema;
+      require_module $schema_name;
+      my $schema = $schema_name->admin_connection;
+      return $schema;
+    }
+  );
+
+  has dir => (
+    is => 'ro',
+    isa => 'Str',
+    default => 'database',
+    documentation => 'The directory to create support files in',
+  );
+}
+
+my $opts = UpgradeDBFlow->new_with_options;
+
 my $dh = DBIx::Class::DeploymentHandler->new({
-   schema              => $schema,
-   script_directory    => "$FindBin::Bin/../../database",
+   schema              => $opts->_schema,
+   script_directory    => $opts->dir,
    databases           => 'MySQL',
    sql_translator_args => { add_drop_table => 0 },
 });
- 
+
 $dh->upgrade;
